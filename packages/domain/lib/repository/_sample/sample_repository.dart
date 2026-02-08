1d7b03a35e1511e26dd884b27a14226d86d8eeb8
watanabe-y-dev <watanabe.y.dev@gmail.com>
2026-02-08 17:36:28 +0900

    feat: サンプルリポジトリインターフェースを追加

---
+++ packages/domain/lib/repository/_sample/sample_repository.dart
@@ -0,0 +1,11 @@
+import 'package:model/_sample/sample.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'sample_repository.g.dart';
+
+@riverpod
+SampleRepository sampleRepository(Ref ref) => throw UnimplementedError();
+
+abstract interface class SampleRepository {
+  Future<List<Sample>> fetchSamples();
+}
