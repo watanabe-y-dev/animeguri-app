712cf0941cbb8868edf3a9e6d4bf211c036946cd
watanabe-y-dev <watanabe.y.dev@gmail.com>
2026-02-09 00:48:26 +0900

    feat: SamplePageViewModel に Effect と Action の処理を実装

--- packages/ui/lib/page/_sample/viewmodel.dart
+++ packages/ui/lib/page/_sample/viewmodel.dart
@@ -1,25 +1,65 @@
+import 'dart:async';
+
 import 'package:domain/notifier/_sample/sample_notifier.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:model/_sample/sample.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:ui/core/effect.dart';
 
+part 'viewmodel.contract.dart';
 part 'viewmodel.freezed.dart';
 part 'viewmodel.g.dart';
-
-@freezed
-abstract class SamplePageState with _$SamplePageState {
-  const factory SamplePageState({
-    required List<Sample> samples,
-  }) = _SamplePageState;
-}
 
 @riverpod
 class SamplePageViewModel extends _$SamplePageViewModel {
   @override
   Future<SamplePageState> build() async {
     final samples = await ref.watch(sampleProvider.future);
-    return SamplePageState(samples: samples);
+    return SamplePageState(
+      samples: samples,
+      effect: const SamplePageEffect.none(),
+    );
   }
 
-  void tapAddButton() {}
+  void onAction(SamplePageAction action) {
+    action.when(
+      tapAddButton: () => _addSample().ignore(),
+    );
+  }
+
+  void _effect(SamplePageEffect effect) {
+    ref.read(samplePageEffectHolderProvider.notifier).onEffect(effect);
+  }
+
+  Future<void> _addSample() async {
+    try {
+      await ref.read(sampleProvider.notifier).add();
+      _effect(
+        const SamplePageEffect.showSuccessToast(
+          message: 'Sample added successfully',
+        ),
+      );
+    } on Exception catch (_) {
+      _effect(
+        const SamplePageEffect.showErrorToast(message: 'Failed to add sample'),
+      );
+    }
+  }
+}
+
+@riverpod
+class SamplePageEffectHolder extends _$SamplePageEffectHolder {
+  @override
+  SamplePageEffect build() {
+    return const SamplePageEffect.none();
+  }
+
+  // ignore: use_setters_to_change_properties
+  void onEffect(SamplePageEffect effect) {
+    state = effect;
+  }
+
+  void reset() {
+    state = const SamplePageEffect.none();
+  }
 }
