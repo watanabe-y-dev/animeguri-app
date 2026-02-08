5e585fa20648a47fe08a67132cbef08a61d23ec9
watanabe-y-dev <watanabe.y.dev@gmail.com>
2026-02-09 00:48:21 +0900

    feat: SamplePage の State, Action, Effect を定義

---
+++ packages/ui/lib/page/_sample/viewmodel.contract.dart
@@ -0,0 +1,25 @@
+part of 'viewmodel.dart';
+
+@freezed
+abstract class SamplePageState with _$SamplePageState {
+  const factory SamplePageState({
+    required List<Sample> samples,
+    required SamplePageEffect effect,
+  }) = _SamplePageState;
+}
+
+@freezed
+sealed class SamplePageAction with _$SamplePageAction {
+  const factory SamplePageAction.tapAddButton() = _SamplePageActionTapAddButton;
+}
+
+@freezed
+sealed class SamplePageEffect with _$SamplePageEffect implements UiEffect {
+  const factory SamplePageEffect.none() = None;
+  const factory SamplePageEffect.showSuccessToast({
+    required String message,
+  }) = ShowSuccessToast;
+  const factory SamplePageEffect.showErrorToast({
+    required String message,
+  }) = ShowErrorToast;
+}
